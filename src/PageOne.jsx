4db3b74f6815d905e0052e8a0cf3573ce62cf9c9
Alliphy <ruebaker29@gmail.com>
2024-08-24 15:46:14 -0600

    HTML skeleton fleshed out

--- src/PageOne.jsx
+++ src/PageOne.jsx
@@ -1,10 +1,24 @@
-
 export default function PageOne() {
-
-
   return (
     <>
       Page One
+      <div>
+        <section>
+          <p>Send me a message</p>
+          <p>
+            Feel free to get in touch with me with anything related to
+            CODINGSPACE or you can just say hi. I will get back to you as soon
+            as I can.
+          </p>
+        </section>
+        <form>
+          <input placeholder="Full Name"></input>
+          <input placeholder="Email Address"></input>
+          <input placeholder="Subject"></input>
+          <input placeholder="Message"></input>
+          <button placeholder="Send" type="submit"></button>
+        </form>
+      </div>
     </>
   );
 }
