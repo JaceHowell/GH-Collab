5d3870a5edeea5046bd1c383861499d6ea845fcd
Alliphy <ruebaker29@gmail.com>
2024-08-24 16:28:04 -0600

    removed extra padding tag

--- src/PageOne.jsx
+++ src/PageOne.jsx
@@ -3,7 +3,7 @@ export default function PageOne() {
     <>
       <div className="flex justify-center w-screen h-screen bg-indigo-500">
         <div className="flex flex-col w-2/5 h-3/4 p-20 mt-20 bg-white rounded">
-          <section className="p-5 p-5 pr-5 items-center text-center">
+          <section className="p-5 pr-5 items-center text-center">
             <p className="p-5 text-3xl"> Send me a message</p>
             <p className="">
               Feel free to get in touch with me with anything related to
