2f6150cf5bbff20c298e2ef7d960e0ce05799ca5
Alliphy <ruebaker29@gmail.com>
2024-08-24 15:57:35 -0600

    everything is centered and padding added

--- src/PageOne.jsx
+++ src/PageOne.jsx
@@ -1,23 +1,26 @@
 export default function PageOne() {
   return (
     <>
-      Page One
-      <div>
-        <section>
-          <p>Send me a message</p>
-          <p>
-            Feel free to get in touch with me with anything related to
-            CODINGSPACE or you can just say hi. I will get back to you as soon
-            as I can.
-          </p>
-        </section>
-        <form>
-          <input placeholder="Full Name"></input>
-          <input placeholder="Email Address"></input>
-          <input placeholder="Subject"></input>
-          <input placeholder="Message"></input>
-          <button placeholder="Send" type="submit"></button>
-        </form>
+      <div className="flex justify-center">
+        <center className="flex flex-col w-3/4 p-20">
+          <section className="p-5">
+            <p className="p-5"> Send me a message</p>
+            <p>
+              Feel free to get in touch with me with anything related to
+              CODINGSPACE or you can just say hi. I will get back to you as soon
+              as I can.
+            </p>
+          </section>
+          <form className="flex flex-col items-center p-5 text-center">
+            <input className="p-5" placeholder="Full Name"></input>
+            <input className="p-5" placeholder="Email Address"></input>
+            <input className="p-5" placeholder="Subject"></input>
+            <input className="p-5" placeholder="Message"></input>
+            <button placeholder="Send" type="submit">
+              Send
+            </button>
+          </form>
+        </center>
       </div>
     </>
   );
